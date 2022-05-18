b3ecb0ce3cda7287f93df89d7a9ea85a13ee3efe
adiziel <aditya.pathak.plus@gmail.com>
2022-05-18 14:04:27 +0530

    Add: in setup.py install_requires added, version upgraded to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='im_botposter',
-  version = "0.0.1",
+  version = "0.0.2",
   url='https://github.com/Adiziel/Imposter',
   description='Im_poster is a Telegram Posts Management Tool',
   long_description= 'Imposter is a telegram bot which creates a post with title and message with and index to track that post, in telegram channel or group in which this bot has been added.',
@@ -11,4 +11,5 @@ setup(
   author_email='aditya.pathak.plus@gmail.com',
   packages=find_packages(),
   keywords=['python', 'telegram', 'telegram bot'],
+  install_requires=['requests', 'json']
 )
