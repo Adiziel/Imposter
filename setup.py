beb871e5d91e2022e42368da9518d073ee3b326c
adiziel <aditya.pathak.plus@gmail.com>
2022-05-18 14:20:20 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from hashlib import md5
 from setuptools import setup, find_packages
 
+
+def readme():
+  with open('README.md') as f:
+    return f.read()
+
 setup(
   name='im_botposter',
   version = "0.0.2",
   url='https://github.com/Adiziel/Imposter',
   description='Im_poster is a Telegram Posts Management Tool',
-  long_description= 'Imposter is a telegram bot which creates a post with title and message with and index to track that post, in telegram channel or group in which this bot has been added.',
+  long_description= readme(),
   author='Adiziel',
   author_email='aditya.pathak.plus@gmail.com',
   packages=find_packages(),
